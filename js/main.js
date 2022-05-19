c619fa801b60a30d690bc419572057e995f05955
Dave86dev <davidochando1986@gmail.com>
2022-05-19 13:01:02 +0200

    ES6 Conceptos básicos

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+// //Desestructuración por asignación en arrays
+// let xenoblade = ["Shulk","Raym","Fiora","Melia"];
+
+// let [primer,segon,tercer,cuart] = xenoblade;
+
+// console.log(segon,cuart);
+
+
+// //Desestructuración en asignación por objetos`
+// let marc = { primer: "messi", segon: "xavi", tercer: "benzema"};
+
+// let { primer: jugador1, segon: jugador2, tercer: jugador3 } = marc;
+
+// console.log(jugador1,jugador2,jugador3);
+
+
+// //Interpolación
+
+// // let nombre = "Lionel";
+
+// // console.log(`Hola, me llamo ${nombre}`);
+
+
+// // let padre = document.getElementById("padre");
+
+// // const llename = () => {
+
+// //     let nombre = prompt("Dime un nombre");
+
+// //     padre.classList.add("multa");
+
+// //     padre.innerHTML = `<strong>${nombre}</strong>`;
+// // };
+
+
+
+//FOR OF
+
+// let marc = ["LOTR", "LOL", "Beyond good & evil", "Star Wars: Battlefront 2"];
+
+// for (let joc of marc) {
+//     console.log(joc);
+// };
+
+//FOR IN (sobretodo se usa cuando hay que iterar sobre un objeto o array);
+
+// let apellidos = ['perez', 'garcia', 'gomez'];
+
+// for (let maria in apellidos) {
+//     // console.log('apellidos[' + index + '] = ' + apellidos[index]);
+//     console.log(maria);
+// };
